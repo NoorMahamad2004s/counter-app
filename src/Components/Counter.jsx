e70377183b0b594144c348614451cadae1f441ea
NoorMahamad <noormahamad.s21@gmail.com>
2024-05-10 19:31:48 +0530

    CounterApp

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,92 +2,94 @@
 // functional component based implementation for counter app
 
 
-// import '../Components/Counter.css';
+import '../Components/Counter.css';
 
-// import { useState } from 'react';
+import { useState } from 'react';
 
-// const Counter = ()=>{
+const Counter = ()=>{
 
-//     const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-//     return(
-//         <div className="container">
-//             <h2 className="heading">Counter App</h2>
-//             <p className="counter">Count: {count}</p>
+    return(
+        <div className="container">
+            <h2 className="heading">Counter App</h2>
+            <p className="counter">Count: {count}</p>
 
-//             <div className="btns">
+            <div className="btns">
 
-//                 <button type="button" className='increment' 
-//                 onClick={()=>{setCount(count+1)}}>
-//                 Increment
-//                 </button>
+                <button type="button" className='increment' 
+                onClick={()=>{setCount(count+1)}}>
+                Increment
+                </button>
 
-//                 <button type="button" className='decrement'
-//                 onClick={()=>{setCount(count-1)}}>
-//                 Decrement
-//                 </button>
+                <button type="button" className='decrement'
+                onClick={()=>{setCount(count-1)}}>
+                Decrement
+                </button>
                 
-//             </div>
-//         </div>
-//     )
-// }
+            </div>
+        </div>
+    )
+}
 
-// export default Counter;
+export default Counter;
+
+
 
 
 // class component based implementation for counter app
 
 
-import { Component } from 'react';
-import '../Components/Counter.css';
+// import { Component } from 'react';
+// import '../Components/Counter.css';
 
-class Counter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 0
-        };
-    }
+// class Counter extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             count: 0
+//         };
+//     }
 
-    handleIncrement = () => {
-        this.setState(prevState => ({
-            count: prevState.count + 1
-        }));
-    };
+//     handleIncrement = () => {
+//         this.setState(prevState => ({
+//             count: prevState.count + 1
+//         }));
+//     };
 
-    handleDecrement = () => {
-        this.setState(prevState => ({
-            count: prevState.count - 1
-        }));
-    };
+//     handleDecrement = () => {
+//         this.setState(prevState => ({
+//             count: prevState.count - 1
+//         }));
+//     };
 
-    render() {
-        const { count } = this.state;
+//     render() {
+//         const { count } = this.state;
 
-        return (
-            <div className="container">
-                <h2 className="heading">Counter App</h2>
-                <p className="counter">Count: {count}</p>
+//         return (
+//             <div className="container">
+//                 <h2 className="heading">Counter App</h2>
+//                 <p className="counter">Count: {count}</p>
 
-                <div className="btns">
-                    <button
-                        type="button"
-                        className="increment"
-                        onClick={this.handleIncrement}
-                    >
-                        Increment
-                    </button>
-                    <button
-                        type="button"
-                        className="decrement"
-                        onClick={this.handleDecrement}
-                    >
-                        Decrement
-                    </button>
-                </div>
-            </div>
-        );
-    }
-}
+//                 <div className="btns">
+//                     <button
+//                         type="button"
+//                         className="increment"
+//                         onClick={this.handleIncrement}
+//                     >
+//                         Increment
+//                     </button>
+//                     <button
+//                         type="button"
+//                         className="decrement"
+//                         onClick={this.handleDecrement}
+//                     >
+//                         Decrement
+//                     </button>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
 
-export default Counter;
+// export default Counter;
