fe071fb67593e274313fc125e13ddfbe9d52ce00
NoorMahamad <noormahamad.s21@gmail.com>
2024-05-10 19:23:48 +0530

    CounterApp

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,32 +1,93 @@
 
+// functional component based implementation for counter app
+
+
+// import '../Components/Counter.css';
+
+// import { useState } from 'react';
+
+// const Counter = ()=>{
+
+//     const [count, setCount] = useState(0);
+
+//     return(
+//         <div className="container">
+//             <h2 className="heading">Counter App</h2>
+//             <p className="counter">Count: {count}</p>
+
+//             <div className="btns">
+
+//                 <button type="button" className='increment' 
+//                 onClick={()=>{setCount(count+1)}}>
+//                 Increment
+//                 </button>
+
+//                 <button type="button" className='decrement'
+//                 onClick={()=>{setCount(count-1)}}>
+//                 Decrement
+//                 </button>
+                
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Counter;
+
+
+// class component based implementation for counter app
+
+
+import { Component } from 'react';
 import '../Components/Counter.css';
 
-import { useState } from 'react';
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
 
-const Counter = ()=>{
+    handleIncrement = () => {
+        this.setState(prevState => ({
+            count: prevState.count + 1
+        }));
+    };
 
-    const [count, setCount] = useState(0);
+    handleDecrement = () => {
+        this.setState(prevState => ({
+            count: prevState.count - 1
+        }));
+    };
 
-    return(
-        <div className="container">
-            <h2 className="heading">Counter App</h2>
-            <p className="counter">Count: {count}</p>
+    render() {
+        const { count } = this.state;
 
-            <div className="btns">
+        return (
+            <div className="container">
+                <h2 className="heading">Counter App</h2>
+                <p className="counter">Count: {count}</p>
 
-                <button type="button" className='increment' 
-                onClick={()=>{setCount(count+1)}}>
-                Increment
-                </button>
-
-                <button type="button" className='decrement'
-                onClick={()=>{setCount(count-1)}}>
-                Decrement
-                </button>
-                
+                <div className="btns">
+                    <button
+                        type="button"
+                        className="increment"
+                        onClick={this.handleIncrement}
+                    >
+                        Increment
+                    </button>
+                    <button
+                        type="button"
+                        className="decrement"
+                        onClick={this.handleDecrement}
+                    >
+                        Decrement
+                    </button>
+                </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
 
 export default Counter;
